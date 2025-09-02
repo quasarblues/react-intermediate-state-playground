b8d3e3df9f70c9380bf2e5e0811aa75f8b97e918
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-09-02 22:50:38 +0900

    Refactored function. Added comments. Added reset btn.

--- src/ScoreKeeper.jsx
+++ src/ScoreKeeper.jsx
@@ -1,19 +1,25 @@
+// Working with objects in state
 import { useState } from "react";
 
 export default function ScoreKeeper() {
     const [scores, setScores] = useState({ p1Score: 0, p2Score: 0 });
 
+    // Function declaration
     function addP1() {
-        setScores((prevScores) => {
-            return { ...prevScores, p1Score: prevScores.p1Score + 1 };
+        setScores(current => {
+            return { ...current, p1Score: scores.p1Score + 1 };
         });
     }
 
-    function addP2() {
-        setScores((prevScores) => {
-            return { ...prevScores, p2Score: prevScores.p2Score + 1 };
+    // Function expression
+    const addP2 = () => setScores(current => ({ ...current, p2Score: scores.p2Score + 1 }));
+
+    function resetGame() {
+        setScores(current => {
+            return { ...current, p1Score: 0, p2Score: 0 };
         });
     }
+
     return (
         <div>
             <h2>Score Keeper!</h2>
@@ -21,6 +27,7 @@ export default function ScoreKeeper() {
             <h3>P2 score is {scores.p2Score}</h3>
             <button onClick={addP1}>+1 Player 1</button>
             <button onClick={addP2}>+2 Player 2</button>
+            <button onClick={resetGame}>Reset Game</button>
         </div>
     )
 }
