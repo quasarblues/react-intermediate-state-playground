d2cb6ab5397e109972764cfecabf5fc8a7105f5b
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-09-03 18:11:47 +0900

    Added UUID

--- src/Emoji.jsx
+++ src/Emoji.jsx
@@ -1,17 +1,23 @@
 // Practicing arrays in state
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Emoji() {
-    const [emojis, setEmojis] = useState(["🥸"]);
+    const [emojis, setEmojis] = useState([
+        { id: uuidv4(), emoji: '🥸' },
+        { id: uuidv4(), emoji: '😒' }
+    ]);
+
     const addEmoji = () => {
-        setEmojis(current => [...current, "🥸"])
+        setEmojis(prevArry => [...prevArry, { id: uuidv4(), emoji: '🥸' }]);
     };
+
     return (
         <div>
-            <h2>Emoji Clicker</h2>
-            {emojis.map(e => (
-                <span style={{ fontSize: '2rem' }}>{e}</span>
-            ))}
+            <h2>Emoji Time</h2>
+            {emojis.map((e) =>
+                <span key={e.id} style={{ fontSize: '2rem', cursor: 'pointer' }}>{e.emoji}</span>
+            )}
             <button onClick={addEmoji} style={{ display: 'block', margin: '0 auto' }}>Add Emoji</button>
         </div>
     )
